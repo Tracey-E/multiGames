1ec48d711a95709865823053ecca74e53f20b1a9
storms22 <traceye5@aol.com>
2022-03-02 18:11:07 +0000

    grid finished

--- src/games/wordGames/wordsearch/Wordsearch.js
+++ src/games/wordGames/wordsearch/Wordsearch.js
@@ -49,7 +49,6 @@ export class Wordsearch extends React.Component {
     this.setState({
       newList: (this.newList = this.state.listOfWords.map((words) => words)),
     });
-    
   };
 
   /** create random grid to put words in */
@@ -109,97 +108,206 @@ export class Wordsearch extends React.Component {
   };
 
   wordFit = () => {
-   while(this.newList.length > 0) {
+    while (this.newList.length > 0) {
       /**get longest word in list */
       this.newList.sort(function (a, b) {
         return b.length - a.length;
       });
-    
+
       this.possStartCell();
       //var i = this.newList[0].length;
-      var word = this.newList[0]
-      
-   
+      var word = this.newList[0];
+
       /**get startcell poss see if enough cells to fit word */
       /**left to right placement */
-       var colSum = this.startCellPosition(this.startCell).column + (word.length);
+      var columnStart = this.startCellPosition(this.startCell).column;
+
+      var colSum = columnStart + word.length;
       /**top to bottom placement */
-      var rowSum = this.startCellPosition(this.startCell).row + (word.length);
-  
+      var rowStart = this.startCellPosition(this.startCell).row;
+      var rowSum = rowStart + word.length;
 
       if (colSum < this.gWidth) {
-      
-        this.rowLetterPlace(word);
-      
-        }  else if (rowSum < this.rHeight) {
-       
-        this.colLetterPlace(word);
-     
+        this.rowLetterPlaceCheck(this.startCell);
+      } else if (rowSum < this.rHeight) {
+        this.colLetterPlaceCheck(this.startCell);
       }
-   
+    }
+    if (this.newList.length === 0) {
+      this.blankSpaceFill();
     }
   };
+  rowLetterPlaceCheck = (x) => {
+    var cellStart = x;
+    //if (word.length > 0) {
+    //var x = this.startCell;
+    //for (let i = 0; i < word.length; i++) {
+    while (this.newList[0].length !== "undefined") {
+      var word = this.newList[0];
 
-  /** place in grid row */
-  rowLetterPlace = (word) => {
-    if (word.length > 0) {
-      var x = this.startCell;
-      for (let i = 0; i < word.length; i++) {
-        var wordSplit = word.split("");
-       
-        console.log(word);
-      }
-    }
-    var letter 
-          while (typeof (  letter = wordSplit.shift()) !== "undefined") {
-            var firstCell = document.getElementById(x);
-            console.log(letter)
-            if (firstCell.innerText === "" || firstCell.innerText === letter) {
-              console.log(firstCell.innerText)
-              firstCell.innerText = letter;
-              x = x + 1;
-             // eslint-disable-next-line no-unused-vars
-             var bin = this.newList.shift()
-             console.log(bin)
-            } else {
-              this.wordFit();
-            
-            }
-        
-      
-    }
-  };
-  /**to place words in columns */
-  colLetterPlace = (word) => {
-    if (word.length > 0) {
-      var x = this.startCell;
-      for (let i = 0; i < word.length; i++) {
-        var wordSplit = word.split("");
-        
-        console.log(word)
-      }
-    }
-    var letter 
-          while (typeof (letter = wordSplit.shift()) !== "undefined") {
-            var firstCell = document.getElementById(x);
+      //var wordSpliter = word.split(' ');
+      // console.log(wordSpliter)
+
+      var letterSplit = word.split("");
+      var bin1 = [];
+      while (letterSplit.length !== 0) {
+        for (let i = 0; i < letterSplit.length; i++) {
+          cellStart = cellStart + 1;
+          var letter = letterSplit[i];
+
+          var cellToTry = document.getElementById(cellStart);
+
+          if (cellToTry.innerText === "" || cellToTry.innerText === letter) {
+            bin1.push(letterSplit.shift());
+          } else {
+            this.wordFit();
           }
-            if (firstCell.innerText === "" || firstCell.innerText === letter) {
-            
-              firstCell.innerText = letter;
-              x = x + this.gWidth;
-              // eslint-disable-next-line no-unused-vars
-              var bin = this.newList.shift()
-              console.log(bin)
-            } else {
-              console.log(this.newList)
-              this.wordFit();
-            }  
-          
-        
-      
-    
+        }
+      }
+
+      if (bin1.length === word.length) {
+        this.rowLetterPlace(word, x);
+
+        this.newList.shift();
+      }
+    }
   };
 
+  rowLetterPlace = (word, x) => {
+    var cellNumber = x;
+
+    while (word.length > 0) {
+      for (let i = 0; i < word.length; i++) {
+        var letters = word.split();
+        var letterSplit = letters[0].split("");
+
+        while (letterSplit.length > 0) {
+          cellNumber = cellNumber + 1;
+
+          var cell = document.getElementById(cellNumber);
+
+          cell.innerText = letterSplit[0];
+
+          letterSplit.shift();
+        }
+        letters.shift();
+
+        word = "";
+      }
+    }
+
+    this.newList.shift();
+
+    this.wordFit();
+  };
+
+  /**to place words in columns */
+  colLetterPlaceCheck = (x) => {
+    var cellStart = x;
+    //if (word.length > 0) {
+    //var x = this.startCell;
+    //for (let i = 0; i < word.length; i++) {
+    while (this.newList[0].length !== 0) {
+      var word = this.newList[0];
+
+      //var wordSpliter = word.split(' ');
+      // console.log(wordSpliter)
+
+      var letterSplit = word.split("");
+      var bin1 = [];
+      while (letterSplit.length !== 0) {
+        for (let i = 0; i < letterSplit.length; i++) {
+          cellStart = cellStart + this.gWidth;
+          var letter = letterSplit[i];
+
+          var cellToTry = document.getElementById(cellStart);
+
+          if (cellToTry.innerText === "" || cellToTry.innerText === letter) {
+            bin1.push(letterSplit.shift());
+          } else {
+            this.wordFit();
+          }
+        }
+      }
+
+      if (bin1.length === word.length) {
+        this.columnLetterPlace(word, x);
+
+        this.newList.shift();
+      }
+    }
+  };
+
+  columnLetterPlace = (word, x) => {
+    var cellNumber = x;
+
+    while (word.length > 0) {
+      for (let i = 0; i < word.length; i++) {
+        var letters = word.split();
+        var letterSplit = letters[0].split("");
+
+        while (letterSplit.length > 0) {
+          cellNumber = cellNumber + this.gWidth;
+
+          var cell = document.getElementById(cellNumber);
+
+          cell.innerText = letterSplit[0];
+
+          letterSplit.shift();
+        }
+        letters.shift();
+
+        word = "";
+      }
+    }
+
+    this.newList.shift();
+
+    this.wordFit();
+  };
+
+  blankSpaceFill = () => {
+    const alphabet = [
+      "a",
+      "b",
+      "c",
+      "d",
+      "e",
+      "f",
+      "g",
+      "h",
+      "i",
+      "j",
+      "k",
+      "l",
+      "m",
+      "n",
+      "o",
+      "p",
+      "q",
+      "r",
+      "s",
+      "t",
+      "u",
+      "v",
+      "w",
+      "x",
+      "y",
+      "z",
+    ];
+    const cellNum = this.gWidth * this.rHeight;
+
+    for (let i = 0; i < cellNum; i++) {
+      var checkCells = document.getElementById(i);
+
+      if (checkCells.innerText === "") {
+        const randomLetter =
+          alphabet[Math.floor(Math.random() * alphabet.length)];
+
+        checkCells.innerText = randomLetter;
+      }
+    }
+  };
   /**user needs to highlight  words ,words in list marked as found */
 
   /**possible hint option of showing  1 first letter of hidden word */
