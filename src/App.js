64ec624ade1d431060b1abab63ae221c208c501a
storms22 <traceye5@aol.com>
2022-05-27 19:00:45 +0100

    update

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ import { PuzzlePage } from "./pages/puzzleGamePage/puzzleGamesPage";
 import { WordPage } from "./pages/wordGamesPage/wordPage";
 
 /**game imports */
-import { Unscramble } from "./games/wordGames/unscramble/Unscramble.js";
+import { Unscramble } from "./games/wordGames/unscramble/unscramble.js";
 import { Wordsearch } from "./games/wordGames/wordsearch/Wordsearch";
 import MouseEscape from "./games/puzzleGames/EscapeBox.js";
 import { NumberGuesser } from "./games/puzzleGames/numberGuesser/numberGuesser";
