8cd9fdb4cdca106191c1b6e7c9dd599cc79cd61d
storms22 <traceye5@aol.com>
2022-05-29 13:11:50 +0100

    updated

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
 import * as React from "react";
+import { Suspense } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { StrictMode } from "react";
 import { createRoot  } from "react-dom/client";
 import "./index.css";
-import App from "./App";
+//import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+const App = React.lazy(() => import("./App"));
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 root.render(
   <StrictMode>
+     <Suspense fallback={<div>Loading...</div>}>
     <Router>
       <App />
     </Router>
+    </Suspense>
   </StrictMode>
 );
 // If you want to start measuring performance in your app, pass a function
